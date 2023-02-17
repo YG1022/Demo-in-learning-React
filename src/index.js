855191f233c7ff3a2c363f76242c915a46454304
YG1022 <958065532@qq.com>
2023-02-17 13:22:28 +0800

    [yinge]feat: create a ts demo as test3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
+import Test3 from "./ts_test/test3";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {false && <App />}
+    <Test3 />
   </React.StrictMode>
 );
 
---
+++ src/ts_test/test3.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Test3 = () => {
+  const [status, setStatus] = useState<boolean>(false);
+  const toggleStatus = () => {
+    setStatus(!status);
+  };
+
+  return (
+    <div>
+      <span>This is Test3!</span>
+      <br />
+      <span>{status ? "Hey!!!" : "What???"}</span>
+      <div>
+        <button onClick={toggleStatus}>Toggle</button>
+      </div>
+    </div>
+  );
+};
+
+export default Test3;
