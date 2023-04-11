2f4099dab576a7d016a8cf6d24c923504d241186
YG1022 <958065532@qq.com>
2023-04-11 10:24:02 +0800

    chore: update this log

--- src/design_pattern/this_bind.js
+++ src/design_pattern/this_bind.js
@@ -8,3 +8,6 @@ console.log(obj.getName()) // obj
 
 const getName = obj.getName
 console.log(getName()) // undefined
+obj.name = 'obj2'
+console.log(getName()) // undefined
+console.log(obj.getName()) // obj2
