c99896f457f6c3fda4d0054901c1b07fbcf7e525
YG1022 <958065532@qq.com>
2023-04-11 11:06:23 +0800

    feat: import life circle

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import './App.css'
-import { Provider } from 'react-redux'
-import toolkitStore from './state_management/redux_toolkit_demo/store'
-import Redux_toolkit_demo from './state_management/redux_toolkit_demo/redux_toolkit_demo'
-import queryStore from './state_management/redux_toolkit_query_demo/queryStore'
-import Redux_toolkit_query_demo from './state_management/redux_toolkit_query_demo/redux_toolkit_query_demo'
-import Redux_demo, { reduxStore } from './state_management/redux_demo'
-import Recoil_demo from './state_management/recoil_demo'
-import Zustand_demo from './state_management/zustand_demo/zustand_demo'
-import { RecoilRoot } from 'recoil'
+import LifeCircle from './design_pattern/life_circle'
 import React from 'react'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
+        {/* <Provider store={reduxStore}>
+          <Redux_demo />
+        </Provider>
+        <hr />
+        <Provider store={toolkitStore}>
+          <Redux_toolkit_demo />
+        </Provider>
+        <hr />
+        <Provider store={queryStore}>
+          <Redux_toolkit_query_demo />
+        </Provider>
+        <hr />
         <RecoilRoot>
-          <Provider store={reduxStore}>
-            <Redux_demo />
-          </Provider>
-          <hr />
-          <Provider store={toolkitStore}>
-            <Redux_toolkit_demo />
-          </Provider>
-          <hr />
-          <Provider store={queryStore}>
-            <Redux_toolkit_query_demo />
-          </Provider>
-          <hr />
           <Recoil_demo />
-          <hr />
-          <Zustand_demo />
         </RecoilRoot>
+        <hr />
+        <Zustand_demo /> */}
+        <LifeCircle />
       </header>
     </div>
   )
